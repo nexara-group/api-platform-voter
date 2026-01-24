fadc51352d476357567498b77b1508de70542ba4
Pavol Fišer <pavolfiser1@gmail.com>
2026-01-24 22:00:48 +0100

    feat: add autoconfiguration for AutoConfiguredCrudVoter via prepend()
    - Implement PrependExtensionInterface in NexaraApiPlatformVoterExtension
    - Use registerForAutoconfiguration() to auto-inject VoterRegistry and MetadataFactory
    - This applies to all AutoConfiguredCrudVoter instances in the application
    - Users no longer need to manually configure _instanceof in services.yaml

--- src/DependencyInjection/NexaraApiPlatformVoterExtension.php
+++ src/DependencyInjection/NexaraApiPlatformVoterExtension.php
@@ -4,13 +4,26 @@ declare(strict_types=1);
 
 namespace Nexara\ApiPlatformVoter\DependencyInjection;
 
+use Nexara\ApiPlatformVoter\Security\Voter\AutoConfiguredCrudVoter;
+use Nexara\ApiPlatformVoter\Security\VoterRegistry;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 
-final class NexaraApiPlatformVoterExtension extends Extension
+final class NexaraApiPlatformVoterExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container): void
+    {
+        // Register autoconfiguration for AutoConfiguredCrudVoter
+        // This will apply to all services in the application that extend AutoConfiguredCrudVoter
+        $container->registerForAutoconfiguration(AutoConfiguredCrudVoter::class)
+            ->addMethodCall('setVoterRegistry', [new Reference(VoterRegistry::class)])
+            ->addMethodCall('setMetadataFactory', [new Reference('api_platform.metadata.resource.metadata_collection_factory')]);
+    }
+
     public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
